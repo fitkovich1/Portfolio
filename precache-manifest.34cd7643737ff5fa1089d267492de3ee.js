59ee6de26bad9d2912d0f0796afb2f84ed64ad7e
fitkovich1 <fitkovich1@gmai.com>
2019-12-06 17:25:37 +0300

    Updates

---
+++ precache-manifest.34cd7643737ff5fa1089d267492de3ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c146bb08371c3f4855b09a990ea98c83",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "db70788179c1f884d149",
+    "url": "/portfolio/static/css/main.e92c89d2.chunk.css"
+  },
+  {
+    "revision": "77ec2b634495966ce2a4",
+    "url": "/portfolio/static/js/2.91d249ef.chunk.js"
+  },
+  {
+    "revision": "db70788179c1f884d149",
+    "url": "/portfolio/static/js/main.5b9369e0.chunk.js"
+  },
+  {
+    "revision": "18e3ecba04a23774b98f",
+    "url": "/portfolio/static/js/runtime-main.d52ae8c2.js"
+  }
+]);
